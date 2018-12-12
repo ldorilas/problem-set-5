eee1f378207b30a31e67ac62dd35ff3e9c115137
ldorilas <43239931+ldorilas@users.noreply.github.com>
2018-12-11 23:20:38 -0500

    tester

--- src/Tester.java
+++ src/Tester.java
@@ -10,8 +10,11 @@ public class Tester {
 	 * @param args
 	 */
 	
+	public Tester(String[] accounts, FileReader fr, BufferedReader br, BufferedWriter write, float balance, long accountNum, float withdraw, float deposit, int pin, String birthdate, int phoneNum, String firstName, String lName, String address, String city, String state, int zipCode) {
+		super(accounts, fr, br, write, balance, accountNumber, withdraw, deposit, pin, birthdate, phone, firstName, lastName, address, city, state, zip);
+	}
 	public static void main(String[] args) {
-		
+		ATM.menu();
 		/*
 		 * Rather than hard coding one or more BankAccount objects, you'll need to read them in
 		 * from our very primitive database (i.e., a flat-file). After making changes, of course,
